ebe0f21b18f4c917a8d60873b59480d3885eaa29
Emil Johansson <emil.sweden@gmail.com>
2020-06-21 10:47:22 +0200

    Catch non-zero return code in python.

--- pycmpw.py
+++ pycmpw.py
@@ -18,17 +18,19 @@ def password(secret,
     c_result_type = ctypes.c_char_p(site_result_type.encode('utf-8'))
     result_type_size = len(site_result_type.encode('utf-8')) + 1
     password_buffer = ctypes.create_string_buffer(_MAX_SIZE_PASSWORD)
-    _cmpw.password(c_secret,
-                   secret_size,
-                   c_site_name,
-                   site_name_size,
-                   c_name,
-                   name_size,
-                   c_result_type,
-                   result_type_size,
-                   counter,
-                   password_buffer,
-                   21)
+    return_code = _cmpw.password(c_secret,
+                                 secret_size,
+                                 c_site_name,
+                                 site_name_size,
+                                 c_name,
+                                 name_size,
+                                 c_result_type,
+                                 result_type_size,
+                                 counter,
+                                 password_buffer,
+                                 21)
+    if return_code != 0:
+        raise Exception("Failed to generate password")
     return password_buffer.value.decode('utf-8')
 
 

5dcec9773177dbca18cd41c648edcf78e034c69d
Emil Johansson <emil.sweden@gmail.com>
2020-06-21 10:22:14 +0200

    Clean up main.c

--- main.c
+++ main.c
@@ -20,23 +20,4 @@ int main() {
            sizeof(password_buffer));
   printf("%s\n", password_buffer);
   return 0;
-  /*
-  const char secret[] = "password";
-  const char name[] = "name";
-  const char site_name[] = "example.org";
-  uint32_t counter = 1;
-  uint8_t key[64];
-  main_key(secret, sizeof(secret),
-           name, sizeof(name),
-           key, sizeof(key));
-  uint8_t site_identity[32];
-  site_key(site_name, sizeof(site_name), key, counter, site_identity);
-  char site_password_buffer[MAX_SIZE_PASSWORD];
-  site_password(site_identity,
-                "Long",
-                sizeof("Long"),
-                site_password_buffer,
-                MAX_SIZE_PASSWORD);
-  printf("%s\n", site_password_buffer);
-  */
 }
